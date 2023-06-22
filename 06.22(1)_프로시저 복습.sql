1430e1762d92f81301660c3ca5f40b0eeb644dff
limJaemin99 <limjaemin99@gmail.com>
2023-06-22 15:33:56 +0900

    JDBC - Day08 트리거
    06.22(1)_프로시저 복습
    06.22(2)_트리거

---
+++ 06.22(1)_프로시저 복습.sql
@@ -0,0 +1,83 @@
+-- 자주 사용하는 테이블 따로 만들어놓기
+CREATE TABLE p_buy
+AS
+SELECT * FROM TBL_BUY tb
+ORDER BY customid;
+
+SELECT * FROM p_buy;
+
+-- 웹 애플리케이션(인터넷 환경) 개발 할 때,
+	-- (JDBC에서)사용자가 원하는 기능(DAO) 요청 하나에 sql을 1개씩만 실행을 합니다.
+-- 프로시저를 이용하면, 요청 한번에 대해 많은 SQL을 실행할 수 있습니다.
+
+-- 데이터베이스 관점에서는 '무결성'(정확성)을 유지할 수 있는 방법.
+
+
+
+-- 실행을 위해서 시퀀스 생성, money 컬럼 추가
+create sequence pbuy_seq start with 1008;
+
+ALTER TABLE P_BUY ADD money NUMBER;
+
+-- 프로시저에서 트랜잭션을 관리하는 예시
+CREATE OR REPLACE PROCEDURE proc_set_money(
+	acustomid IN varchar2,	-- 회원ID	-- 입력 매개변수 IN
+	apcode IN varchar2,		-- 상품 코드
+	acnt IN NUMBER,			-- 수량
+	isSuccess OUT varchar2	-- 출력 매개변수 OUT. 트랜잭션 처리 성공여부 저장
+)
+IS 
+	vseq NUMBER;	-- 변수 선언
+	vprice NUMBER;
+BEGIN
+	INSERT INTO P_BUY(buy_seq,customid,pcode,quantity,buy_date)
+		VALUES (pbuy_seq.nextval, acustomid,apcode,acnt,sysdate);	-- 매개변수값으로 추가
+		
+	SELECT pbuy_seq.currval	-- 현재 시퀀스 값 조회
+		INTO vseq
+	FROM dual;
+	
+	SELECT price
+		INTO vprice
+	FROM TBL_PRODUCT tp
+	WHERE pcode=apcode;	-- 상품 코드에 대한 가격 조회
+	
+	UPDATE P_BUY
+	SET money = vprice * quantity
+	WHERE buy_seq = vseq;	-- 위 INSERT 한 데이터로 가격 * 수량 수식 구해서 money 컬럼에 입력
+
+	dbms_output.put_line('실행 성공');
+	isSuccess := 'success';		-- ★ 프로시저에서 일반 변수 대입문 기호 [ := ] ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+	COMMIT;
+	EXCEPTION	-- EXCEPTION 추가하여 처리 ▶ 메시지 출력, ROLLBACK
+		WHEN OTHERS THEN 
+		dbms_output.put_line('실행 실패');
+		ROLLBACK;	-- 오류가 발생한 SQL 앞의 INSERT,UPDATE,DELETE 를 취소
+		isSuccess := 'fail';
+END;
+
+
+-- [시퀀스값을 되돌릴 수 있는 방법은 없음]
+-- fail 일 경우 시퀀스값은 그대로 지나가고, 그게 싫다면 다음 두가지 방법을 사용할 수 있음
+-- 1. exception 발생시 시퀀스 삭제 및 vseq-1값으로 재생성
+-- 2. 시퀀스 대신 max(buy_seq)+1 사용
+
+-- 롤백 시나리오 만들기
+ALTER TABLE P_BUY DROP (money);
+-- money 에 10000 미만의 값이 들어가면 오류 발생 ▶ 롤백
+ALTER TABLE P_BUY ADD money NUMBER(7) CHECK (money >= 10000);
+
+SELECT * FROM P_BUY pb;
+
+-- 실행 예시
+DECLARE
+	vresult varchar2(20);
+BEGIN
+-- 메시지는 'fail' 이고 p_buy 테이블 INSERT 도 입력값 없어야 함.
+-- 10000원이 넘으므로 'success'	
+	proc_set_money('AAA','3MCRY',5,vresult);
+	dbms_output.put_line('결과 : '||vresult);
+END;
+
+SELECT * FROM P_BUY pb;
+
---
+++ 06.22(2)_트리거.sql
@@ -0,0 +1,69 @@
+-- ★ 트리거 ★
+
+-- 트리거(trigger)는 데이터베이스 시스템에서 
+-- 데이터의 `입력, 갱신, 삭제() 등의 이벤트`가 발생할때마다 '자동적으로 수행되는' [사용자 정의 프로시저]이다.
+-- 특정 테이블에 속해있는 객체
+/*
+
+	CREATE OR REPLACE TRIGGER 트리거이름
+	BEFORE | AFTER                  -- 동작 시점
+	INSERT | UPDATE | DELETE ON 테이블명        -- 테이블과 해당 C,R,D 지정
+	[FOR EACH ROW]      -- 행 트리거. 테이블 행 단위 변화에 대해 동작하기.
+	[WHEN(조건)]      -- 특정컬럼에 대한 값 조건
+	DECLARE    -- 변수선언
+	...
+	BEGIN
+	...
+	[EXCEPTION]  -- 예외사항
+	END; 
+
+ */
+
+-- 트리거의 장점 : 원하는 작업을 자동으로 실행해줌.
+	-- 데이터베이스 테이블 관리를 위한 목적.
+
+-- 트리거 만들기
+CREATE OR REPLACE TRIGGER cancle_buy
+AFTER DELETE ON p_buy	-- p_buy 테이블 대상으로 DELETE 실행한 후에 동작한다.
+FOR EACH ROW	-- 적용하는 행이 여러개일때, 각 행에 대해 실행함
+				-- :OLD UPDATE 또는 DELETE 하기 전 값, :NEW 는 INSERT 한 값
+BEGIN
+	-- 구매 취소(p_buy 테이블에서 삭제)한 데이터 tri_temp 임시 테이블에 insert : 여러행에 대한 작업(행 트리거)
+	INSERT INTO tri_temp
+	values(:OLD.buy_seq, :OLD.customid,:OLD.pcode , :OLD.quantity, :OLD.buy_date, :OLD.money);
+END;
+
+-- 임시 테이블 tri_temp 만들기
+CREATE TABLE tri_temp
+as
+SELECT * FROM P_BUY
+WHERE buy_seq = 0;	-- WHERE buy_seq = 0 ▶ 컬럼만 복사할때(데이터는 가져오지 않을때) 사용
+-- 확인
+SELECT * FROM tri_temp;
+SELECT * FROM P_BUY;
+
+-- 트리거 동작시키기
+DELETE FROM P_BUY WHERE customid = 'AAA';
+
+
+
+-- 테이블 관리를 위한 트리거 : 예시로 UPDATE 와 DELETE 명령을 할 수 없는 시간을 정함.
+CREATE OR REPLACE TRIGGER secure_custom
+BEFORE UPDATE OR DELETE ON tbl_custom	-- 트리거 동작하는 테이블,SQL과 시점
+BEGIN
+	IF TO_CHAR(SYSDATE , 'hh24:mi') BETWEEN '12:00' AND '16:00' THEN
+		raise_application_error(-20000,'지금 오후 12시 ~ 16시는 작업할 수 없습니다.');
+		-- ORA-20000: 지금 오후 12시 ~ 16시는 작업할 수 없습니다. ▶ 오류 메세지 이렇게 나옴.
+	END IF;
+END;
+-- 트리거 동작 테스트
+DELETE FROM TBL_CUSTOM WHERE customer_id = 'momo';
+
+-- momo 추가용
+INSERT INTO TBL_CUSTOM (customer_id,name) VALUES ('momo','모모');
+SELECT * FROM TBL_CUSTOM tc ;
+
+
+
+-- 내일 수업할 내용 혼자 해보기
+	-- 아이디, 물품코드, 수량 INSERT 하면 총 가격까지 추가해서 출력하게 트리거 만들기
